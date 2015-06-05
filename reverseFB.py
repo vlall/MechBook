ad0405a4de49130a3104cbddc06a78223c4fcd1e
Vishal <vlall@users.noreply.github.com>
2015-06-05 09:42:14 -0400

    Update reverseFB.py

--- reverseFB.py
+++ reverseFB.py
@@ -10,19 +10,34 @@ class Identity:
 
 	def __init__(self, filename):
 		dataIn = open(filename, 'rU')
-		phoneBook = []
 		self.filename = filename
+		phoneBook = []
 		self.phoneBook = phoneBook
+		self.failedCounter = 0
 		for row in dataIn:
 			cells = row.split(',')
 			if 'phone' in row:
-				phoneBook.append(cells[2].rstrip())		
+				self.phoneBook.append(cells[2].rstrip())		
+		print '*** Reading CSV ****'
 
-	def get_Name(self, number):
+	def open_File(self, number):
 		self.number = number
 		sitetest = browser.open('https://www.facebook.com/search/str/%%20%s/keywords_top' % str(number))
 		site = sitetest.read()
-		username = site
+		return site
+
+	def site_Test(self, number = 8102934256):
+		username = self.open_File(number)
+		if '<div class="_5d-5">' in username:
+			nameSplit1 = username.split('<div class="_5d-5">')[1]
+			nameSplit2 = nameSplit1.split('<div class="_glm">')[0]
+			fullName = nameSplit2.split('</div>')[0]
+			return 'SUCCESS'
+		else:
+			return 'FAILED'
+
+	def get_Name(self, number):
+		username = self.open_File(number)
 		# Scrape between '<div class="_5d-5">' and '<div class="_glm">' for facebook profile name
 		if '<div class="_5d-5">' in username:
 			nameSplit1 = username.split('<div class="_5d-5">')[1]
@@ -32,12 +47,8 @@ class Identity:
 		else:
 			return 'None'
 
-
 	def get_URL(self, number):
-		self.number = number
-		sitetest = browser.open('https://www.facebook.com/search/str/%%20%s/keywords_top' % str(number))
-		site = sitetest.read()
-		username = site
+		username = self.open_File(number)
 		# Scrape between '<div class="_5d-5">' and '<div class="_glm">' for facebook profile name
 		if '<div class="_5d-5">' in username:
 			nameSplit1 = username.split('<div class="_gll"><a href="')[1]
@@ -45,7 +56,9 @@ class Identity:
 			fullURL = nameSplit2.split('?ref=br_rs">')[0]
 			return fullURL
 		else:
+			self.failedCounter +=1
 			return 'None'
+
 
 	def get_phoneBook(self):
 		print "Phone numbers found: %s" % (len(self.phoneBook))
@@ -59,8 +72,8 @@ if __name__ == '__main__':
 	browser.open("https://www.facebook.com/login.php")
 	browser.select_form(nr=0)
 	# Enter Facebook information of user that's performing the search
-	browser.form['email'] = 'User_Email'
-	browser.form['pass'] = 'User_Password'
+	browser.form['email'] = 'YOUR_EMAIL'
+	browser.form['pass'] = 'YOUR_PASS'
 	response = browser.submit()
 	findUsers = Identity('hadoop-small.csv')
 	#print response.read()
@@ -68,13 +81,23 @@ if __name__ == '__main__':
 	#print findUsers.get_phoneBook()
 	identityList = []
 	for i in findUsers.phoneBook:
-		if findUsers.get_Name(i) != 'None':
-			if findUsers.get_Name(i) not in identityList:
-				identityList.append([findUsers.get_Name(i),findUsers.get_URL(i)])
-			print '%s, %s' % (findUsers.get_Name(i),findUsers.get_URL(i))
+		name = findUsers.get_Name(i)
+		url = findUsers.get_URL(i)
+		if name != 'None':
+			if [name,url] not in identityList:
+				identityList.append([name,url])
+				print '%s, %s' % (name,url)
+		else:
+			print 'Searching...'
 		time.sleep(1)
-
+	
 	print identityList
+	print '%d unique identities found.\n %d numbers unidentitified.' % (len(identityList),findUsers.failedCounter)
 	text_file = open("data_Out.csv", "w")
-	text_file.write(identityList)
+	text_file.write(str(identityList))
 	text_file.close()
+
+
+
+
+
