5a5dbffe3d20018feb1e10d58971c237215ae9b1
Vishal <vlall@users.noreply.github.com>
2015-06-06 12:59:40 -0400

    Update and rename utils.py to fb_utils.py

--- mechbook/fb_utils.py
+++ mechbook/fb_utils.py
@@ -3,12 +3,14 @@ import mechanize
 
 class Load_FB:
 
+	#Facebook Mechanize session
 	def __init__(self):
 		with open('../config.json', 'r') as file:
 			config = json.load(file)
 		_email = config['email']
 		_pass = config['pass']
-		_data = config['datafile']
+		# Set Location of data file
+		_data = '../' + config['datafile']
 		self._email = _email
 		self._pass = _pass
 		self._data = _data
@@ -18,22 +20,14 @@ class Load_FB:
 		browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US)     AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7')]
 		browser.open("https://www.facebook.com/login.php")
 		browser.select_form(nr=0)
+		
 		# Enter Facebook information of user that's performing the search
 		browser.form['email'] = _email
 		browser.form['pass'] = _pass
 		response = browser.submit()
 		self.browser = browser
-		#print response.read()
-
-	def get_Browser(self):
-		return self.browser
 		
-	def data_up(self):
-		findUsers = Identity('../' + self._data)
-		return findUsers
-
 if __name__ == '__main__': 
-	testingUtils = Load_FB()
-	run = testingUtils.data_up()
-	print run.site_Test(8102934256)
- 
+
+	#Debugging
+	print response.read()
