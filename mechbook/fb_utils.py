13b531d03f59748a65841027fa14578d45c509b2
Vishal <vlall@users.noreply.github.com>
2015-06-06 13:20:02 -0400

    Update fb_utils.py

--- mechbook/fb_utils.py
+++ mechbook/fb_utils.py
@@ -20,7 +20,7 @@ class Load_FB:
 		browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US)     AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7')]
 		browser.open("https://www.facebook.com/login.php")
 		browser.select_form(nr=0)
-		
+
 		# Enter Facebook information of user that's performing the search
 		browser.form['email'] = _email
 		browser.form['pass'] = _pass
@@ -28,6 +28,5 @@ class Load_FB:
 		self.browser = browser
 		
 if __name__ == '__main__': 
-
 	#Debugging
 	print response.read()
